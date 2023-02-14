d5b2ad9d0bb771331899cfd77caba0436c789b80
Fabian Untermoser <f.untermoser@gmx.at>
2023-02-14 12:06:12 +0100

    chore(app): format

---
+++ app/src/main/java/com/example/examplespringbootpostgres/ExampleSpringBootPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringBootPostgresApplication.class, args);
+    }
+
+}
---
+++ app/src/test/java/com/example/examplespringbootpostgres/ExampleSpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplespringbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
